49e6387c5730a98d0f95fa137a9229caf645d960
Fernando Krell <fernando@eian.io>
2019-02-11 16:05:52 +0100

    Adding chaum-pedersen for multiple commitments equality proof

--- src/chaum_perdersen.rs
+++ src/chaum_perdersen.rs
@@ -9,7 +9,9 @@ use curve25519_dalek::scalar::Scalar;
 use blake2::{Blake2b, Digest};
 use curve25519_dalek::ristretto::RistrettoPoint;
 
-fn compute_challenge(context: Vec<&CompressedRistretto>) -> Scalar{
+use curve25519_dalek::traits::Identity;
+
+fn compute_challenge(context: &Vec<&CompressedRistretto>) -> Scalar{
     /*! I compute zk challenges for Dlog based proof. The challenge is a hash of the
     current context of the proof*/
     let mut hasher = Blake2b::new();
@@ -17,6 +19,17 @@ fn compute_challenge(context: Vec<&CompressedRistretto>) -> Scalar{
     for point in context.iter(){
         hasher.input((*point).as_bytes());
     }
+
+    Scalar::from_hash(hasher)
+}
+
+fn compute_sub_challenge(challenge: &Scalar, i: u32) -> Scalar{
+    /*! I compute zk sub challenges for multiple Dlog based proofs.
+    The sub-challenge is a hash of the challenge and the position i of the sub-challenge*/
+    let mut hasher = Blake2b::new();
+
+    hasher.input(challenge.as_bytes());
+    hasher.input(u32_to_bigendian_u8array(i));
 
     Scalar::from_hash(hasher)
 }
@@ -57,7 +70,7 @@ pub fn chaum_pedersen_prove_eq<R: CryptoRng + Rng>(
     let c4 = pedersen_gens.commit(r3, r5).compress();
 
     let c = compute_challenge(
-        vec![commitment1, commitment2, &c3, &c4]);
+        &vec![commitment1, commitment2, &c3, &c4]);
 
     let z1 = c*value + r3;
     let z2 = c*r1 + r4;
@@ -66,6 +79,14 @@ pub fn chaum_pedersen_prove_eq<R: CryptoRng + Rng>(
     ChaumPedersenCommitmentEqProof{
         c3,c4,z1,z2,z3
     }
+}
+
+fn get_fake_zero_commitment() -> CompressedRistretto {
+    RistrettoPoint::identity().compress()
+}
+
+fn get_fake_zero_commitment_blinding() -> Scalar {
+    Scalar::from(0u8)
 }
 
 pub fn chaum_pedersen_eq_verify(
@@ -83,13 +104,86 @@ pub fn chaum_pedersen_eq_verify(
     let g = &pc_gens.B;
     let h = &pc_gens.B_blinding;
 
-    let c = compute_challenge(vec![c1, c2, &proof.c3, &proof.c4]);
+    let c = compute_challenge(&vec![c1, c2, &proof.c3, &proof.c4]);
 
     let mut vrfy_ok = c3_d + c*c1_d == z1*g + z2*h;
     vrfy_ok = vrfy_ok && c4_d + c*c2_d == z1*g + z3*h;
     Ok(vrfy_ok)
 
 }
+
+pub fn chaum_pedersen_prove_multiple_eq<R: CryptoRng +  Rng>(
+    prng: &mut R,
+    pedersen_gens: &PedersenGens,
+    commitments: &Vec<&CompressedRistretto>,
+    blinding_factors: Vec<&Scalar>) -> ChaumPedersenCommitmentEqProof
+{
+    /*! I produce a proof that all commitments are to the same value.
+     *
+    */
+    let k = compute_challenge(&commitments);
+    let mut d = RistrettoPoint::identity();
+    let mut z = Scalar::from(0u8);
+    let c1 = commitments.get(0).unwrap();
+    let c1_decompressed = (*c1).decompress().unwrap();
+    let r1 = blinding_factors.get(0).unwrap();
+    for i in 1..commitments.len(){
+        let ci = commitments.get(i).unwrap();
+        let ai = compute_sub_challenge(&k, i as u32);
+        let ci_decompressed = ci.decompress().unwrap();
+        let di = ai * (c1_decompressed - ci_decompressed);
+        let ri = blinding_factors.get(i).unwrap();
+        let zi = ai * (*r1 - *ri);
+        d = d + di;
+        z = z + zi;
+    }
+
+
+    //TODO can we produce proof to zero commitment in a more direct way?
+    //produce fake commitment to 0 for chaum pedersen commitment
+    let proof = chaum_pedersen_prove_eq(prng,
+                                        pedersen_gens,
+                                        &Scalar::from(0u8),
+                                        &d.compress(),
+                                        &get_fake_zero_commitment(),
+                                        &z,
+                                        &get_fake_zero_commitment_blinding());
+    proof
+}
+
+pub fn chaum_pedersen_verify_multiple_eq(
+    pedersen_gens: &PedersenGens,
+    commitments: &Vec<&CompressedRistretto>,
+    proof: &ChaumPedersenCommitmentEqProof) -> Result<bool, ZeiError>
+{
+    /*! I produce a proof that all commitments are to the same value.
+     *
+    */
+    let k = compute_challenge(&commitments);
+    let mut d = RistrettoPoint::identity();
+    let c1 = commitments.get(0)?;
+    let c1_decompressed = c1.decompress()?;
+    for i in 1..commitments.len(){
+        let ci = commitments.get(i)?;
+        let ai = compute_sub_challenge(&k, i as u32);
+        let ci_decompressed = ci.decompress()?;
+        let di = ai * (c1_decompressed - ci_decompressed);
+        d = d + di;
+    }
+
+
+    //TODO can we produce proof to zero commitment in a more direct way?
+    //produce fake commitment to 0 for chaum pedersen commitment
+    let vrfy_ok = chaum_pedersen_eq_verify(
+        pedersen_gens,
+        &d.compress(),
+        &get_fake_zero_commitment(),
+        proof,
+    )?;
+
+    Ok(vrfy_ok)
+}
+
 
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq, Default)]
 pub struct DlogProof{
@@ -104,7 +198,7 @@ pub fn prove_knowledge_dlog<R: CryptoRng + Rng>(
     dlog: &Scalar) -> DlogProof{
     let u = Scalar::random(prng);
     let proof_commitment = (u*base).compress();
-    let challenge = compute_challenge(vec![&base.compress(), &proof_commitment, point]);
+    let challenge = compute_challenge(&vec![&base.compress(), &proof_commitment, point]);
     let response = challenge * dlog + u;
 
     DlogProof {
@@ -119,7 +213,7 @@ pub fn verify_proof_of_knowledge_dlog(
     proof:&DlogProof) -> Result<bool, ZeiError>{
 
     let challenge = compute_challenge(
-        vec![&base.compress(), &proof.proof_commitment, point]);
+        &vec![&base.compress(), &proof.proof_commitment, point]);
 
     let dpoint = point.decompress()?;
     let dproof_commit = proof.proof_commitment.decompress()?;
@@ -140,13 +234,13 @@ pub fn prove_multiple_knowledge_dlog<R: CryptoRng + Rng>(
     let base_compressed = base.compress();
     let mut context = vec![&base_compressed, &proof_commitment];
     context.extend_from_slice(points);
-    let challenge = compute_challenge(context);
+    let challenge = compute_challenge(&context);
     let mut response = u;
     for i in 0..dlogs.len() {
         let mut hasher = Blake2b::new();
         hasher.input(challenge.as_bytes());
         hasher.input(u32_to_bigendian_u8array(i as u32));
-        let challenge_i = Scalar::from_hash(hasher);
+        let challenge_i = compute_sub_challenge(&challenge, i as u32);
         response = response + challenge_i * dlogs[i];
     }
     DlogProof {
@@ -163,7 +257,7 @@ pub fn verify_multiple_knowledge_dlog(
     let base_compressed = base.compress();
     let mut context = vec![&base_compressed, &proof.proof_commitment];
     context.extend_from_slice(points);
-    let challenge = compute_challenge(context);
+    let challenge = compute_challenge(&context);
     let mut check = proof.proof_commitment.decompress()?;
     for i in 0..points.len() {
         let mut hasher = Blake2b::new();
@@ -321,6 +415,56 @@ mod test {
             &c1,
             &c3,
             &proof).unwrap());
+    }
+
+    #[test]
+    fn test_chaum_perdersen_multiple_eq_proof(){
+        let mut csprng: ChaChaRng;
+        csprng = ChaChaRng::from_seed([0u8; 32]);
+        let pc_gens = PedersenGens::default();
+        let value1 = Scalar::from(16u8);
+        let value2 = Scalar::from(32u8);
+        let bf1 = Scalar::from(10u8);
+        let bf2 = Scalar::from(100u8);
+        let bf3 = Scalar::from(1000u32);
+        let pedersen_bases = PedersenGens::default();
+        let c1 = pedersen_bases.commit(value1, bf1).compress();
+        let c2 = pedersen_bases.commit(value2, bf2).compress();
+        let c3 = pedersen_bases.commit(value1, bf3).compress();
+
+        let com_vec = vec![&c1,&c2,&c3];
+        let blind_vec = vec![&bf1,&bf2,&bf3];
+
+        let proof = chaum_pedersen_prove_multiple_eq(
+            &mut csprng,
+            &pc_gens,
+            &com_vec,
+            blind_vec);
+
+        assert_eq!(false, chaum_pedersen_verify_multiple_eq(
+            &pc_gens,
+            &com_vec,
+            &proof).unwrap());
+
+        let c1 = pedersen_bases.commit(value1, bf1).compress();
+        let c2 = pedersen_bases.commit(value1, bf2).compress();
+        let c3 = pedersen_bases.commit(value1, bf3).compress();
+
+        let com_vec = vec![&c1,&c2,&c3];
+        let blind_vec = vec![&bf1,&bf2,&bf3];
+
+        let proof = chaum_pedersen_prove_multiple_eq(
+            &mut csprng,
+            &pc_gens,
+            &com_vec,
+            blind_vec);
+
+        assert_eq!(true, chaum_pedersen_verify_multiple_eq(
+            &pc_gens,
+            &com_vec,
+            &proof).unwrap());
+
+
     }
 
     #[test]
