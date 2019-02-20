59049b9a57b4bd5181807852bca7a965edd8ca86
Fernando Krell <fernando@eian.io>
2019-02-20 11:53:18 +0100

    Code cleanup

--- src/utxo_transaction.rs
+++ src/utxo_transaction.rs
@@ -1,67 +1,70 @@
-use curve25519_dalek::ristretto::RistrettoPoint;
+use blake2::{Blake2b,Digest};
+use bulletproofs::{PedersenGens, RangeProof};
+use core::borrow::Borrow;
+use crate::asset::Asset;
+use crate::errors::Error as ZeiError;
+use crate::encryption::ZeiRistrettoCipher;
+use crate::proofs::chaum_perdersen::{chaum_pedersen_prove_multiple_eq,
+                                     chaum_pedersen_verify_multiple_eq, ChaumPedersenCommitmentEqProof};
+use crate::setup::{BULLET_PROOF_RANGE,PublicParams};
+use crate::utils::{u64_to_bigendian_u8array, u8_bigendian_slice_to_u64};
+use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 use curve25519_dalek::scalar::Scalar;
 use curve25519_dalek::traits::Identity;
-use crate::asset::Asset;
+use merlin::Transcript;
 use rand::CryptoRng;
 use rand::Rng;
-use crate::setup::PublicParams;
-use bulletproofs::{PedersenGens, RangeProof};
-use blake2::{Blake2b,Digest};
-use crate::errors::Error as ZeiError;
-use crate::proofs::chaum_perdersen::{chaum_pedersen_prove_multiple_eq, ChaumPedersenCommitmentEqProof};
-use curve25519_dalek::ristretto::CompressedRistretto;
-use merlin::Transcript;
-use crate::setup::BULLET_PROOF_RANGE;
-use crate::proofs::chaum_perdersen::chaum_pedersen_verify_multiple_eq;
-use crate::encryption::ZeiRistrettoCipher;
-use schnorr::Signature;
+use schnorr::{PublicKey, SecretKey,Signature};
 use std::collections::HashSet;
-use schnorr::PublicKey;
-use schnorr::SecretKey;
-use core::borrow::Borrow;
-use crate::utils::u64_to_bigendian_u8array;
-use crate::utils::u8_bigendian_slice_to_u64;
 
 
 #[derive(Default)]
 pub struct TxAddressParams{
-    amount: u64, //input or output amount
-    amount_commitment: Option<CompressedRistretto>, //input or output balance
-    amount_blinding: Option<Scalar>, //none for output
+    /// Address parameters used as input and output to create transactiona
+    amount: u64, // Input or output amount
+    amount_commitment: Option<CompressedRistretto>, // Input or output balance
+    amount_blinding: Option<Scalar>, // None for output
     asset_type: String,
-    asset_type_commitment: Option<CompressedRistretto>, //None if non confidential asset or account is new, or Utxo model
-    asset_type_blinding: Option<Scalar>, //None if non confidential asset or account is new or Utxo model
+    asset_type_commitment: Option<CompressedRistretto>, // None if non confidential asset or
+                                                        // account is new, or Utxo model
+    asset_type_blinding: Option<Scalar>, // None if non confidential asset or
+                                         // account is new or Utxo model
     public_key: PublicKey,
-    secret_key: Option<SecretKey>,
+    secret_key: Option<SecretKey>, //None for output account
 }
 
-pub struct TxPublicAddressInfo{
-    amount: Option<u64>, //None only if confidential
-    amount_commitment: Option<CompressedRistretto>, //None if not confidential balance
-    asset_type: Option<String>, //None only if confidential asset
-    asset_type_commitment: Option<CompressedRistretto>,  //None if not confidential balance
-    public_key: PublicKey, //source or destination
+pub struct TxPublicFields {
+    /// Address information for input and output that is safe to add to transaction
+    amount: Option<u64>, // None only if confidential
+    amount_commitment: Option<CompressedRistretto>, // None if not confidential balance
+    asset_type: Option<String>, // None only if confidential asset
+    asset_type_commitment: Option<CompressedRistretto>,  // None if not confidential balance
+    public_key: PublicKey, // source or destination
 }
 
-pub struct TxDestinationInfo{
-    public_info: TxPublicAddressInfo,
+pub struct TxOutput {
+    /// Output structure for output
+    public: TxPublicFields,
     lock_box: Option<ZeiRistrettoCipher>,
 }
 
 pub struct TxProofs{
+    /// Proof to be included in transactions
     range_proof: Option<RangeProof>,
     asset_proof: Option<(ChaumPedersenCommitmentEqProof, ChaumPedersenCommitmentEqProof)>,
 }
 
 pub struct TxBody{
-    source_info: Vec<TxPublicAddressInfo>,
-    destination_info: Vec<TxDestinationInfo>,
+    /// Transaction body structure
+    input: Vec<TxPublicFields>,
+    output: Vec<TxOutput>,
     proofs: TxProofs,
     confidential_amount:bool,
     confidential_asset: bool,
 }
 
 pub struct Tx{
+    /// Transaction structure
     body: TxBody,
     signatures: Vec<Signature>,
 }
@@ -78,26 +81,25 @@ impl Tx{
         let pc_gens = PedersenGens::default();
 
         //output values to be build
-        let mut range_proof = None;
-        let mut asset_proof = None;
-        let mut source_info = Vec::new();
-        let mut destination_info= Vec::new();
+        let mut tx_range_proof = None;
+        let mut tx_asset_proof = None;
+        let mut tx_input = Vec::new();
+        let mut tx_output= Vec::new();
+        let mut tx_in_amount = vec![];
+        let mut tx_out_amount = vec![];
+        let mut tx_out_amount_com: Vec<Option<CompressedRistretto>>;
+        let mut tx_out_asset_com: Vec<Option<CompressedRistretto>>;
+        // to be encrypted to receiver
+        let mut memo_out_amount_blind = vec![];
+        let mut memo_out_asset_blind: Vec<Option<Scalar>>;
 
-        //tmp values
-        let mut src_amount_option = vec![];
-        let mut dst_amount_option = vec![];
-        let mut dst_amount_blind = vec![];
-        let mut dst_asset_com: Vec<Option<CompressedRistretto>>;
-        let mut dst_asset_blind: Vec<Option<Scalar>>;
-        let mut out_amount_coms_option: Vec<Option<CompressedRistretto>>;
-
-        //extract values from input struct
+        // extract values from input struct
         let src_asset_type_com_option: Vec<Option<CompressedRistretto>> =
             input.iter().map(|x| x.asset_type_commitment).collect();
         let src_pks: Vec<PublicKey> =
             input.iter().map(|x| x.public_key).collect();
 
-        //extract values from output struct
+        // extract values from output struct
         let dst_asset_type_com_option: Vec<Option<CompressedRistretto>> =
             output.iter().map(|x| x.asset_type_commitment).collect();
         let dst_asset_type_blind_option: Vec<Option<Scalar>> =
@@ -105,27 +107,29 @@ impl Tx{
         let destination_public_keys: Vec<PublicKey> =
             output.iter().map(|x| x.public_key ).collect();
 
-        //do amount handling
+        // do amount handling
         if confidential_amount {
-            let (range_pf, out_coms, out_blinds)
+            let (range_proof, out_coms, out_blinds)
                 = Tx::do_confidential_amount_range_proof(prng, input, output)?;
 
-            out_amount_coms_option = out_coms.into_iter().map(|x| Some(x)).collect();
-            range_proof = Some(range_pf);
-            dst_amount_blind = out_blinds;
+            tx_out_amount_com = out_coms.into_iter().map(|x| Some(x)).collect();
+            tx_range_proof = Some(range_proof);
+            memo_out_amount_blind = out_blinds;
 
-            //in confidential amount transaction, amounts are hidden, use None value
+            // in confidential amount transaction, amounts are hidden, use None value
             for _ in 0..input.len(){
-               src_amount_option.push(None);
+               tx_in_amount.push(None);
             }
             for _ in 0..output.len(){
-               dst_amount_option.push(None);
+               tx_out_amount.push(None);
             }
         }
         else{
-            src_amount_option = input.iter().map(|x| Some(x.amount)).collect();
-            dst_amount_option = output.iter().map(|x| Some(x.amount)).collect();
-            out_amount_coms_option = (0..output.len()).map(|_| None).collect();
+            // add plaintext amount to source and destination
+            // out commitmements are None
+            tx_in_amount = input.iter().map(|x| Some(x.amount)).collect();
+            tx_out_amount = output.iter().map(|x| Some(x.amount)).collect();
+            tx_out_amount_com = (0..output.len()).map(|_| None).collect();
         }
 
         if confidential_asset{
@@ -145,24 +149,25 @@ impl Tx{
                 &destination_public_keys,
             )?;
 
-            asset_proof = Some(proof_asset);
-            dst_asset_com = vec![];
-            dst_asset_blind = vec![];
+            tx_asset_proof = Some(proof_asset);
+            tx_out_asset_com = vec![];
+            memo_out_asset_blind = vec![];
             for (x,y) in out_asset_com.iter().zip(out_asset_blind.iter()) {
-                dst_asset_com.push(Some(*x));
-                dst_asset_blind.push(Some(*y))
+                tx_out_asset_com.push(Some(*x));
+                memo_out_asset_blind.push(Some(*y))
             }
         }
         else{
-            dst_asset_com = dst_asset_type_com_option;
-            dst_asset_blind = dst_asset_type_blind_option;
+            // if non-confidential asset, then output asset_com is same as before (None)
+            tx_out_asset_com = dst_asset_type_com_option;
+            memo_out_asset_blind = dst_asset_type_blind_option;
         }
 
         //compute input struct
         for i in 0..input.len(){
-            source_info.push(
-                TxPublicAddressInfo{
-                    amount: src_amount_option[i],
+            tx_input.push(
+                TxPublicFields {
+                    amount: tx_in_amount[i],
                     amount_commitment: input[i].amount_commitment,
                     asset_type: match confidential_asset{ true => None, false => Some(input[i].asset_type.clone())},
                     asset_type_commitment: src_asset_type_com_option[i],
@@ -178,10 +183,10 @@ impl Tx{
                 let mut memo = vec![];
                 if confidential_amount {
                     memo.extend_from_slice(&u64_to_bigendian_u8array(output[i].amount));
-                    memo.extend_from_slice(dst_amount_blind[i].as_bytes());
+                    memo.extend_from_slice(memo_out_amount_blind[i].as_bytes());
                 }
                 if confidential_asset {
-                    memo.extend_from_slice(dst_asset_blind[i].unwrap().as_bytes());
+                    memo.extend_from_slice(memo_out_asset_blind[i].unwrap().as_bytes());
                 }
                 let ciphertext = ZeiRistrettoCipher::encrypt(
                     prng,
@@ -194,16 +199,16 @@ impl Tx{
                 lbox = None;
             }
 
-            destination_info.push(
-                TxDestinationInfo{
-                    public_info: TxPublicAddressInfo{
-                        amount: dst_amount_option[i],
-                        amount_commitment: out_amount_coms_option[i],
+            tx_output.push(
+                TxOutput {
+                    public: TxPublicFields {
+                        amount: tx_out_amount[i],
+                        amount_commitment: tx_out_amount_com[i],
                         asset_type: match confidential_asset {
                             true => None,
                             false => Some(output[i].asset_type.clone()),
                         },
-                        asset_type_commitment: dst_asset_com[i],
+                        asset_type_commitment: tx_out_asset_com[i],
                         public_key: destination_public_keys[i],
                     },
                     lock_box: lbox,
@@ -223,7 +228,7 @@ impl Tx{
             }
         }
 
-        Ok(Tx::build_tx_struct(source_info, destination_info, range_proof, asset_proof, signatures))
+        Ok(Tx::build_tx_struct(tx_input, tx_output, tx_range_proof, tx_asset_proof, signatures))
     }
 
     fn do_confidential_amount_range_proof<R: CryptoRng + Rng>(prng: &mut R,
@@ -363,8 +368,8 @@ impl Tx{
 
 
     fn build_tx_struct(
-        source_info: Vec<TxPublicAddressInfo>,
-        destination_info: Vec<TxDestinationInfo>,
+        source_info: Vec<TxPublicFields>,
+        destination_info: Vec<TxOutput>,
         range_proof: Option<RangeProof>,
         asset_proof: Option<(ChaumPedersenCommitmentEqProof, ChaumPedersenCommitmentEqProof)>,
         signatures: Vec<Signature>) -> Tx
@@ -376,8 +381,8 @@ impl Tx{
             asset_proof,
         };
         let body = TxBody{
-            source_info,
-            destination_info,
+            input: source_info,
+            output: destination_info,
             proofs,
             confidential_amount,
             confidential_asset,
@@ -397,9 +402,9 @@ impl Tx{
             }
         }
         else {
-            let in_amount: Vec<u64> = self.body.source_info.iter().map(|x| x.amount.unwrap()).collect();
+            let in_amount: Vec<u64> = self.body.input.iter().map(|x| x.amount.unwrap()).collect();
             let in_amount_sum = in_amount.iter().sum::<u64>();
-            let out_amount: Vec<u64> = self.body.destination_info.iter().map(|x| x.public_info.amount.unwrap()).collect();
+            let out_amount: Vec<u64> = self.body.output.iter().map(|x| x.public.amount.unwrap()).collect();
             let out_amount_sum = out_amount.iter().sum::<u64>();
 
             if out_amount_sum > in_amount_sum {
@@ -412,16 +417,16 @@ impl Tx{
             return self.verify_confidential_asset();
         }
         //else
-        let asset_id_option = self.body.source_info[0].asset_type.as_ref().unwrap();
-        for x in self.body.source_info.iter(){
+        let asset_id_option = self.body.input[0].asset_type.as_ref().unwrap();
+        for x in self.body.input.iter(){
             let asset_id_option_i = x.asset_type.as_ref().unwrap();
             if asset_id_option_i != asset_id_option {
                 return false;
             }
         }
 
-        for x in self.body.destination_info.iter(){
-            let asset_id_option_i = x.public_info.asset_type.as_ref().unwrap();
+        for x in self.body.output.iter(){
+            let asset_id_option_i = x.public.asset_type.as_ref().unwrap();
             if asset_id_option_i != asset_id_option {
                 return false;
             }
@@ -430,17 +435,17 @@ impl Tx{
     }
 
     fn verify_confidential_amount(&self) -> bool {
-        let num_output = self.body.destination_info.len();
+        let num_output = self.body.output.len();
         let upper_power2 = smallest_greater_power_of_two((num_output + 1) as u32) as usize;
 
         let params = PublicParams::new(upper_power2);
         let mut transcript = Transcript::new(b"Zei Range Proof");
 
-        let input_com: Vec<RistrettoPoint> = self.body.source_info.iter().
+        let input_com: Vec<RistrettoPoint> = self.body.input.iter().
             map(|x| x.amount_commitment.unwrap().decompress().unwrap()).collect();
 
-        let output_com: Vec<RistrettoPoint> = self.body.destination_info.iter().
-            map(|x| x.public_info.amount_commitment.
+        let output_com: Vec<RistrettoPoint> = self.body.output.iter().
+            map(|x| x.public.amount_commitment.
                 unwrap().decompress().unwrap()).collect();
 
         let diff_com = input_com.iter().sum::<RistrettoPoint>() -
@@ -470,11 +475,11 @@ impl Tx{
 
     fn verify_confidential_asset(&self) -> bool{
         let pc_gens = PedersenGens::default();
-        let mut asset_commitments: Vec<CompressedRistretto> = self.body.source_info.iter().
+        let mut asset_commitments: Vec<CompressedRistretto> = self.body.input.iter().
             map(|x| x.asset_type_commitment.unwrap()).collect();
 
-        let out_asset_commitments: Vec<CompressedRistretto> = self.body.destination_info.iter().
-            map(|x| x.public_info.asset_type_commitment.unwrap()).collect();
+        let out_asset_commitments: Vec<CompressedRistretto> = self.body.output.iter().
+            map(|x| x.public.asset_type_commitment.unwrap()).collect();
 
         asset_commitments.extend(out_asset_commitments.iter());
 
@@ -648,7 +653,7 @@ mod test {
         assert_eq!(true, tx.verify(), "Not confidential simple transaction should verify ok");
 
         for i in 0..num_outputs {
-            assert_eq!(None, tx.body.destination_info[i].lock_box);
+            assert_eq!(None, tx.body.output[i].lock_box);
         }
 
         //overflow transfer
@@ -738,7 +743,7 @@ mod test {
         for i in 0..4 {
             let (amount, amount_blind, asset_blind) =
                 Tx::receiver_unlock_memo(
-                    tx.body.destination_info[i].lock_box.as_ref().unwrap(),
+                    tx.body.output[i].lock_box.as_ref().unwrap(),
                     &out_sks[i], false, true).unwrap();
 
             assert_eq!(None, amount, "Conf. asset tx: Decryption should not contain amount");
@@ -746,7 +751,7 @@ mod test {
             let blind_com = pc_gens.commit(Asset { id: String::from(asset_id) }.
                 compute_scalar_hash(), asset_blind.unwrap());
             assert_eq!(blind_com.compress(),
-                       tx.body.destination_info[i].public_info.asset_type_commitment.unwrap(),
+                       tx.body.output[i].public.asset_type_commitment.unwrap(),
                        "Conf. asset tx: Decryption should contain valit asset blinding");
             //TODO what if output blinding was provided (account based)
         }
@@ -816,7 +821,7 @@ mod test {
         //check receivers memos decryption
         for i in 0..num_outputs {
             let (amount, amount_blind, asset_blind) =
-                Tx::receiver_unlock_memo(tx.body.destination_info[i].lock_box.as_ref().unwrap(),
+                Tx::receiver_unlock_memo(tx.body.output[i].lock_box.as_ref().unwrap(),
                                          &out_sks[i],
                                          true, false).unwrap();
 
@@ -828,7 +833,7 @@ mod test {
             let amount_com = pc_gens.commit(Scalar::from(out_amount[i]),
                                             amount_blind.unwrap());
             assert_eq!(amount_com.compress(),
-                       tx.body.destination_info[i].public_info.amount_commitment.unwrap(),
+                       tx.body.output[i].public.amount_commitment.unwrap(),
                        "Conf. amount tx: memo decryption should contain valid amount blinding");
         }
 
