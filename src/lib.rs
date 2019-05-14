67d0c13f675a03e600791bd0c7c42f7c43877e97
Nathan Yospe <nathan@eian.io>
2019-05-13 17:33:10 -0700

    Making things that need to be, public

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@
 #![feature(try_trait)]
 //#![feature(custom_attribute)]
 
-extern crate rand_04;
-extern crate pairing;
-extern crate rustc_serialize;
 extern crate bulletproofs;
 extern crate curve25519_dalek;
 extern crate ed25519_dalek;
@@ -13,26 +10,29 @@ extern crate itertools;
 extern crate merlin;
 extern crate num_bigint;
 extern crate num_traits;
+extern crate pairing;
 extern crate rand;
+extern crate rand_04;
 extern crate rmp_serde;
+extern crate rustc_serialize;
 extern crate serde;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
+extern crate bn;
+extern crate digest;
 extern crate serde_json;
 extern crate sha2;
 extern crate sodiumoxide;
-extern crate bn;
-extern crate digest;
 
-mod errors;
+mod algebra;
 mod setup;
 mod utils;
-mod algebra;
-
 
 //pub mod bls12_381_credentials;
 //pub mod bn_credentials;
-pub mod credentials;
-pub mod proofs;
 pub mod basic_crypto;
+pub mod credentials;
+pub mod errors;
+pub mod proofs;
 pub mod serialization;
 pub mod transfers;
