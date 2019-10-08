f7cd9426fe7294518bd2f662789e76efd34951af
Fernando Krell <fernando@findora.org>
2019-10-08 14:01:16 -0300

    Cleanup lib.rs, updated dalek deps versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,12 @@
 //! Zei: Eian's cryptographic library
-//#![feature(try_from)]
-//#![feature(try_trait)]
-//#![feature(custom_attribute)]
-#![feature(test)]
-extern crate aes_ctr;
-extern crate bulletproofs;
-extern crate curve25519_dalek;
-extern crate ed25519_dalek;
+//#![feature(test)]
+//extern crate aes_ctr;
+
 #[macro_use]
 extern crate itertools;
-extern crate linear_map;
-extern crate merlin;
-extern crate num_bigint;
-extern crate num_traits;
-extern crate pairing;
-extern crate rand;
-extern crate rand_04;
-extern crate rmp_serde;
-extern crate serde;
-#[macro_use]
+
+#[macro_use] //rustc crushes if not used
 extern crate serde_derive;
-extern crate bn;
-extern crate digest;
-extern crate serde_json;
-extern crate sha2;
-extern crate spacesuit;
-extern crate test;
 
 pub mod algebra;
 pub mod basic_crypto;
