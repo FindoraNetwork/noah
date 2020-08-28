6ebe91f5d1cce4f790ff05926fb8fad1ad46574f
Philippe Camacho <philippe@findora.org>
2020-08-28 13:08:02 -0400

    Test for mimc function.

--- crypto/src/basics/hash_functions/mimc.rs
+++ crypto/src/basics/hash_functions/mimc.rs
@@ -66,4 +66,61 @@ pub(crate) fn mimc_feistel(left: &Scalar, right: &Scalar, c: &[Scalar]) -> (Scal
   (xl, xr)
 }
 
-// TODO tests
+#[cfg(test)]
+pub mod test {
+
+  use crate::basics::hash_functions::mimc::MiMCHash;
+  use crate::basics::hash_functions::MTHash;
+  use algebra::groups::Scalar;
+
+  fn check_mimc(level: usize, input: &[&curve25519_dalek::scalar::Scalar], expected_output: &[u8]) {
+    let hash = MiMCHash::new(level);
+
+    let expected_output_scalar = Scalar::from_bytes(expected_output).unwrap();
+    let res = hash.digest(input);
+    assert_eq!(res, expected_output_scalar);
+  }
+
+  #[test]
+  fn mimc() {
+    check_mimc(0,
+               &[],
+               &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0]);
+
+    check_mimc(0,
+               &[&Scalar::from_u64(1)],
+               &[215, 151, 33, 139, 13, 189, 216, 171, 212, 224, 32, 91, 177, 168, 253, 14, 111,
+                 219, 48, 107, 2, 189, 149, 235, 107, 214, 49, 139, 124, 128, 204, 3]);
+
+    check_mimc(1,
+               &[],
+               &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0]);
+
+    check_mimc(1,
+               &[&Scalar::from_u64(1), &Scalar::from_u64(5)],
+               &[84, 187, 110, 168, 34, 210, 33, 51, 131, 110, 106, 108, 78, 131, 241, 207, 73,
+                 110, 220, 110, 33, 219, 61, 27, 11, 203, 171, 90, 220, 83, 134, 15]);
+
+    check_mimc(2,
+               &[&Scalar::from_u64(3),
+                 &Scalar::from_u64(3),
+                 &Scalar::from_u64(10)],
+               &[18, 170, 81, 36, 201, 195, 102, 94, 73, 192, 203, 249, 89, 93, 147, 61, 224,
+                 180, 197, 169, 51, 164, 199, 88, 47, 0, 213, 69, 119, 80, 87, 5]);
+
+    check_mimc(15,
+               &[&Scalar::from_u64(4553),
+                 &Scalar::from_u64(778878),
+                 &Scalar::from_u64(11),
+                 &Scalar::from_u64(45),
+                 &Scalar::from_u64(454),
+                 &Scalar::from_u64(0),
+                 &Scalar::from_u64(33366),
+                 &Scalar::from_u64(4587),
+                 &Scalar::from_u64(11)],
+               &[12, 61, 158, 17, 144, 198, 23, 28, 51, 100, 67, 120, 128, 52, 224, 169, 89, 1,
+                 147, 81, 105, 150, 163, 11, 200, 65, 18, 185, 123, 144, 41, 14]);
+  }
+}
