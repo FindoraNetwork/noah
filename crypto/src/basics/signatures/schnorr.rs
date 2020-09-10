26b4ede6556fd6b1389c4f899873204d63babb4e
Philippe Camacho <philippe@findora.org>
2020-09-10 12:03:40 -0300

    Remove 'Schnorr' prefix in structs names.

--- crypto/src/basics/signatures/schnorr.rs
+++ crypto/src/basics/signatures/schnorr.rs
@@ -28,43 +28,43 @@ const SCALAR_SIZE: usize = 32;
 pub type SchnorrNonce = [u8; SCALAR_SIZE];
 
 #[derive(Serialize, Deserialize, PartialEq, Eq)]
-pub struct SchnorrSecretKey<S> {
+pub struct SecretKey<S> {
   pub(crate) key: S,
   pub(crate) nonce: SchnorrNonce,
 }
 
-impl<S: Scalar> SchnorrSecretKey<S> {
-  pub fn new(key: S, nonce: SchnorrNonce) -> SchnorrSecretKey<S> {
-    SchnorrSecretKey { key, nonce }
+impl<S: Scalar> SecretKey<S> {
+  pub fn new(key: S, nonce: SchnorrNonce) -> SecretKey<S> {
+    SecretKey { key, nonce }
   }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct SchnorrPublicKey<G>(G);
+pub struct PublicKey<G>(G);
 
-impl<G: Group> ZeiFromToBytes for SchnorrPublicKey<G> {
+impl<G: Group> ZeiFromToBytes for PublicKey<G> {
   fn zei_to_bytes(&self) -> Vec<u8> {
     self.0.to_compressed_bytes()
   }
-  fn zei_from_bytes(bytes: &[u8]) -> Result<SchnorrPublicKey<G>, ZeiError> {
+  fn zei_from_bytes(bytes: &[u8]) -> Result<PublicKey<G>, ZeiError> {
     let group_element = G::from_compressed_bytes(bytes);
 
     match group_element {
-      Ok(g) => Ok(SchnorrPublicKey(g)),
+      Ok(g) => Ok(PublicKey(g)),
       _ => Err(ZeiError::ParameterError),
     }
   }
 }
 #[derive(Serialize, Deserialize, PartialEq, Eq)]
-pub struct SchnorrKeyPair<G, S> {
-  sec_key: SchnorrSecretKey<S>,
-  pub_key: SchnorrPublicKey<G>,
+pub struct KeyPair<G, S> {
+  sec_key: SecretKey<S>,
+  pub_key: PublicKey<G>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[allow(non_snake_case)]
 /// A Schnorr signature is composed by some group element R and some scalar s
-pub struct SchnorrSignature<G: Group> {
+pub struct Signature<G: Group> {
   R: G,
   s: G::S,
 }
@@ -73,7 +73,7 @@ pub struct SchnorrSignature<G: Group> {
 pub trait SchnorrTranscript {
   fn update_transcript_with_sig_info<B: AsRef<[u8]>, G: Group>(&mut self,
                                                                msg: &B,
-                                                               pk: &SchnorrPublicKey<G>,
+                                                               pk: &PublicKey<G>,
                                                                commitment: &G);
 
   fn compute_challenge<S: Scalar>(&mut self) -> S;
@@ -82,7 +82,7 @@ pub trait SchnorrTranscript {
 impl SchnorrTranscript for Transcript {
   fn update_transcript_with_sig_info<B: AsRef<[u8]>, G: Group>(&mut self,
                                                                msg: &B,
-                                                               pk: &SchnorrPublicKey<G>,
+                                                               pk: &PublicKey<G>,
                                                                commitment: &G) {
     self.append_message(b"message", msg.as_ref());
     self.append_message(b"public key", &pk.clone().zei_to_bytes());
@@ -99,7 +99,7 @@ impl SchnorrTranscript for Transcript {
 }
 
 #[allow(non_snake_case)]
-impl<G: Group> ZeiFromToBytes for SchnorrSignature<G> {
+impl<G: Group> ZeiFromToBytes for Signature<G> {
   fn zei_to_bytes(&self) -> Vec<u8> {
     let mut v1 = self.R.to_compressed_bytes();
     let mut v2 = self.s.to_bytes();
@@ -107,7 +107,7 @@ impl<G: Group> ZeiFromToBytes for SchnorrSignature<G> {
     v1
   }
 
-  fn zei_from_bytes(bytes_repr: &[u8]) -> Result<SchnorrSignature<G>, ZeiError> {
+  fn zei_from_bytes(bytes_repr: &[u8]) -> Result<Signature<G>, ZeiError> {
     let R = G::from_compressed_bytes(&bytes_repr[..G::COMPRESSED_LEN]);
     if R.is_err() {
       return Err(ZeiError::ParameterError);
@@ -115,7 +115,7 @@ impl<G: Group> ZeiFromToBytes for SchnorrSignature<G> {
     let R = R.unwrap(); // safe unwrap()
     let s = G::S::from_bytes(&bytes_repr[G::COMPRESSED_LEN..]);
     match s {
-      Ok(s) => Ok(SchnorrSignature { R, s }),
+      Ok(s) => Ok(Signature { R, s }),
       _ => Err(ZeiError::DeserializationError),
     }
   }
@@ -123,12 +123,12 @@ impl<G: Group> ZeiFromToBytes for SchnorrSignature<G> {
 
 /// In this naive implementation a multi signature is a list
 /// of  "simple" signatures.
-pub struct SchnorrMultiSignature<G: Group>(Vec<SchnorrSignature<G>>);
+pub struct MultiSignature<G: Group>(Vec<Signature<G>>);
 
 /// Generates a key pair for the Schnorr signature scheme
 /// * `prng` - pseudo-random generator
 /// * `returns` - a key pair
-pub fn schnorr_gen_keys<R: CryptoRng + RngCore, G: Group>(prng: &mut R) -> SchnorrKeyPair<G, G::S> {
+pub fn schnorr_gen_keys<R: CryptoRng + RngCore, G: Group>(prng: &mut R) -> KeyPair<G, G::S> {
   // Private key
   let alpha = G::S::random(prng);
   // Secret nonce:
@@ -139,8 +139,8 @@ pub fn schnorr_gen_keys<R: CryptoRng + RngCore, G: Group>(prng: &mut R) -> Schno
   let base = G::get_base();
   let u = base.mul(&alpha);
 
-  SchnorrKeyPair { sec_key: SchnorrSecretKey::new(alpha, nonce),
-                   pub_key: SchnorrPublicKey(u) }
+  KeyPair { sec_key: SecretKey::new(alpha, nonce),
+            pub_key: PublicKey(u) }
 }
 
 /// Deterministic computation of a scalar based on the secret nonce of the private key.
@@ -167,9 +167,9 @@ fn deterministic_scalar_gen<G: Group>(message: &[u8], nonce: &SchnorrNonce) -> G
 /// * `signing_key` - key pair. Having both public and private key makes the signature computation more efficient
 /// * `message` - sequence of bytes to be signed
 /// * `returns` - a Schnorr signature
-pub fn schnorr_sign<B: AsRef<[u8]>, G: Group>(signing_key: &SchnorrKeyPair<G, G::S>,
+pub fn schnorr_sign<B: AsRef<[u8]>, G: Group>(signing_key: &KeyPair<G, G::S>,
                                               msg: &B)
-                                              -> SchnorrSignature<G> {
+                                              -> Signature<G> {
   let mut transcript = Transcript::new(b"schnorr_sig");
 
   let g = G::get_base();
@@ -185,23 +185,22 @@ pub fn schnorr_sign<B: AsRef<[u8]>, G: Group>(signing_key: &SchnorrKeyPair<G, G:
   let private_key = &(signing_key.sec_key).key;
   let s: G::S = r.add(&c.mul(private_key));
 
-  SchnorrSignature { R, s }
+  Signature { R, s }
 }
 
 /// Computes a signature with key pairs sk_1, sk_2,...,sk_n on a message m
 /// * `signing_keys` - list of key pairs
 /// * `message` - message to be signed
-pub fn schnorr_multisig_sign<B: AsRef<[u8]>, G: Group>(signing_keys: &[SchnorrKeyPair<G,
-                                                                        G::S>],
+pub fn schnorr_multisig_sign<B: AsRef<[u8]>, G: Group>(signing_keys: &[KeyPair<G, G::S>],
                                                        message: &B)
-                                                       -> SchnorrMultiSignature<G> {
+                                                       -> MultiSignature<G> {
   let mut signatures = vec![];
 
   for signing_key in signing_keys {
     let sig = schnorr_sign::<B, G>(&signing_key, &message);
     signatures.push(sig);
   }
-  SchnorrMultiSignature(signatures)
+  MultiSignature(signatures)
 }
 
 /// Verifies a Schnorr signature given a message, a public key
@@ -210,9 +209,9 @@ pub fn schnorr_multisig_sign<B: AsRef<[u8]>, G: Group>(signing_keys: &[SchnorrKe
 /// * `sig` - signature
 /// * `returns` - Nothing if the verification succeeds, an error otherwise
 #[allow(non_snake_case)]
-pub fn schnorr_verify<B: AsRef<[u8]>, G: Group>(pk: &SchnorrPublicKey<G>,
+pub fn schnorr_verify<B: AsRef<[u8]>, G: Group>(pk: &PublicKey<G>,
                                                 msg: &B,
-                                                sig: &SchnorrSignature<G>)
+                                                sig: &Signature<G>)
                                                 -> Result<(), ZeiError> {
   let mut transcript = Transcript::new(b"schnorr_sig");
 
@@ -238,9 +237,9 @@ pub fn schnorr_verify<B: AsRef<[u8]>, G: Group>(pk: &SchnorrPublicKey<G>,
 /// * `msg` - message
 /// * `msig` - multi signature
 /// * `returns` - Nothing if the verification succeeds, an error otherwise
-pub fn schnorr_multisig_verify<B: AsRef<[u8]>, G: Group>(public_keys: &[SchnorrPublicKey<G>],
+pub fn schnorr_multisig_verify<B: AsRef<[u8]>, G: Group>(public_keys: &[PublicKey<G>],
                                                          msg: &B,
-                                                         msig: &SchnorrMultiSignature<G>)
+                                                         msig: &MultiSignature<G>)
                                                          -> Result<(), ZeiError> {
   if public_keys.len() != msig.0.len() || public_keys.is_empty() {
     return Err(ZeiError::ParameterError);
@@ -259,8 +258,7 @@ mod schnorr_sigs {
   mod schnorr_simple_sig {
 
     use crate::basics::signatures::schnorr::{
-      schnorr_gen_keys, schnorr_sign, schnorr_verify, SchnorrKeyPair, SchnorrPublicKey,
-      SchnorrSignature, SCALAR_SIZE,
+      schnorr_gen_keys, schnorr_sign, schnorr_verify, KeyPair, PublicKey, Signature, SCALAR_SIZE,
     };
     use algebra::groups::{Group, GroupArithmetic, One};
     use algebra::jubjub::JubjubGroup;
@@ -273,7 +271,7 @@ mod schnorr_sigs {
       let seed = [0_u8; SCALAR_SIZE];
       let mut prng = rand_chacha::ChaChaRng::from_seed(seed);
 
-      let key_pair: SchnorrKeyPair<G, G::S> = schnorr_gen_keys::<ChaCha20Rng, G>(&mut prng);
+      let key_pair: KeyPair<G, G::S> = schnorr_gen_keys::<ChaCha20Rng, G>(&mut prng);
 
       let message = String::from("message");
 
@@ -283,8 +281,8 @@ mod schnorr_sigs {
       let res = schnorr_verify::<String, G>(&public_key, &message, &sig);
       assert!(res.is_ok());
 
-      let wrong_sig = SchnorrSignature { R: <G as Group>::get_identity(),
-                                         s: <G as GroupArithmetic>::S::one() };
+      let wrong_sig = Signature { R: <G as Group>::get_identity(),
+                                  s: <G as GroupArithmetic>::S::one() };
       let res = schnorr_verify::<String, G>(&public_key, &message, &wrong_sig);
       assert!(res.is_err());
 
@@ -306,19 +304,19 @@ mod schnorr_sigs {
     fn check_from_to_bytes<G: Group>() {
       let seed = [0_u8; SCALAR_SIZE];
       let mut prng = rand_chacha::ChaChaRng::from_seed(seed);
-      let key_pair: SchnorrKeyPair<G, G::S> = schnorr_gen_keys::<ChaCha20Rng, G>(&mut prng);
+      let key_pair: KeyPair<G, G::S> = schnorr_gen_keys::<ChaCha20Rng, G>(&mut prng);
       let message = String::from("message");
       let sig = schnorr_sign::<String, G>(&key_pair, &message);
       let public_key = key_pair.pub_key;
 
       // Public key
       let public_key_bytes = public_key.zei_to_bytes();
-      let public_key_from_bytes = SchnorrPublicKey::zei_from_bytes(&public_key_bytes).unwrap();
+      let public_key_from_bytes = PublicKey::zei_from_bytes(&public_key_bytes).unwrap();
       assert_eq!(public_key, public_key_from_bytes);
 
       // Signature
       let signature_bytes = sig.zei_to_bytes();
-      let signature_from_bytes = SchnorrSignature::zei_from_bytes(&signature_bytes).unwrap();
+      let signature_from_bytes = Signature::zei_from_bytes(&signature_bytes).unwrap();
       assert_eq!(sig, signature_from_bytes);
     }
 
@@ -337,8 +335,8 @@ mod schnorr_sigs {
   mod schnorr_multisig {
 
     use crate::basics::signatures::schnorr::{
-      schnorr_gen_keys, schnorr_multisig_sign, schnorr_multisig_verify, SchnorrKeyPair,
-      SchnorrMultiSignature, SchnorrSignature, SCALAR_SIZE,
+      schnorr_gen_keys, schnorr_multisig_sign, schnorr_multisig_verify, KeyPair, MultiSignature,
+      Signature, SCALAR_SIZE,
     };
     use algebra::groups::{Group, GroupArithmetic, One};
 
@@ -356,7 +354,7 @@ mod schnorr_sigs {
       let mut key_pairs = vec![];
       let mut public_keys = vec![];
       for _ in 0..NUMBER_OF_KEYS {
-        let key_pair: SchnorrKeyPair<G, G::S> = schnorr_gen_keys::<ChaCha20Rng, G>(&mut prng);
+        let key_pair: KeyPair<G, G::S> = schnorr_gen_keys::<ChaCha20Rng, G>(&mut prng);
         let public_key = key_pair.pub_key.clone();
         key_pairs.push(key_pair);
         public_keys.push(public_key);
@@ -369,11 +367,12 @@ mod schnorr_sigs {
       let res = schnorr_multisig_verify::<String, G>(&public_keys, &message, &msig);
       assert!(res.is_ok());
 
-      let wrong_msig: SchnorrMultiSignature<G> = SchnorrMultiSignature(vec![
-                                SchnorrSignature{ R: <G as Group>::get_identity(),
-                                                  s: <G as GroupArithmetic>::S::one()};
-                                3
-                              ]);
+      let wrong_msig: MultiSignature<G> =
+        MultiSignature(vec![
+                         Signature { R: <G as Group>::get_identity(),
+                                     s: <G as GroupArithmetic>::S::one() };
+                         3
+                       ]);
       let res = schnorr_multisig_verify::<String, G>(&public_keys, &message, &wrong_msig);
       assert!(res.is_err());
 
@@ -381,11 +380,11 @@ mod schnorr_sigs {
       let res = schnorr_multisig_verify::<String, G>(&public_keys, &wrong_message, &msig);
       assert_eq!(res, Err(ZeiError::ArgumentVerificationError));
 
-      let too_short_multi_sig = SchnorrMultiSignature(msig.0.clone()[0..2].to_vec());
+      let too_short_multi_sig = MultiSignature(msig.0.clone()[0..2].to_vec());
       let res = schnorr_multisig_verify::<String, G>(&public_keys, &message, &too_short_multi_sig);
       assert_eq!(res, Err(ZeiError::ParameterError));
 
-      let empty_msig = SchnorrMultiSignature(vec![]);
+      let empty_msig = MultiSignature(vec![]);
       let empty_public_keys = vec![];
       let res = schnorr_multisig_verify::<String, G>(&empty_public_keys, &message, &empty_msig);
       assert_eq!(res, Err(ZeiError::ParameterError));
