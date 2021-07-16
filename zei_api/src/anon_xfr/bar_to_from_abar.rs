f7e62546c977bc5f0fa6924caf0b4a078de7bf57
Harshad Patil <harshad0402@gmail.com>
2021-07-16 14:31:35 -0700

    Adding Eq, Clone to structs for txnbuilding on platform

--- zei_api/src/anon_xfr/bar_to_from_abar.rs
+++ zei_api/src/anon_xfr/bar_to_from_abar.rs
@@ -46,7 +46,7 @@ pub struct AbarToBarBody {
     pub proof: ConvertBarAbarProof,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Eq, Clone, PartialEq)]
 pub struct BarToAbarNote {
     pub body: BarToAbarBody,
     pub signature: XfrSignature,
