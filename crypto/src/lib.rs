6941bd50a35a2800c951cc0e5b0f6036d5191b6d
Sun <sun@findora.org>
2022-03-18 20:09:04 +0800

    cleanup unused whitelist

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -18,12 +18,9 @@ pub mod bp_circuits;
 pub mod bp_range_proofs;
 pub mod chaum_pedersen;
 pub mod conf_cred_reveal;
-pub mod group_signatures;
-pub mod merkle_tree;
-// pub mod inner_product_pairing; // TODO back in when BlsGt is serializable
 pub mod field_simulation;
+pub mod group_signatures;
 pub mod pc_eq_rescue_split_verifier_zk_part;
 pub mod pedersen_elgamal;
 pub mod sigma;
 pub mod solvency;
-//pub mod whitelist;
