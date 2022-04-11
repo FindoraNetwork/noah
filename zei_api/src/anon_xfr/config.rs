c2c7e9436cefa3caad22231431e487d6f67c729b
Harshad Patil <harshad0402@gmail.com>
2022-04-11 14:18:50 -0700

    fix TX_FEE_MIN value

--- zei_api/src/anon_xfr/config.rs
+++ zei_api/src/anon_xfr/config.rs
@@ -5,5 +5,5 @@ pub const FEE_TYPE: AssetType = ASSET_TYPE_FRA;
 
 pub const FEE_CALCULATING_FUNC: fn(u32, u32) -> u32 = |x: u32, y: u32| {
     let extra_outputs = y.checked_sub(x).unwrap_or(0);
-    50_0000 + 10_0000 * x + 20_0000 * y + (10_0000 * extra_outputs)
+    50_0000 + 10_0000 * x + 20_0000 * y + (10_000 * extra_outputs)
 };
