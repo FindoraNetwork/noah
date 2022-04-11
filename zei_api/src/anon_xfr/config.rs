ec29bc00a39f12ac9d87b660ecc70c6ebb8d3082
Harshad Patil <harshad0402@gmail.com>
2022-04-11 12:45:30 -0700

    Updating FEE_CALCULATING_FUNC to include extra fee for more outputs than inputs

--- zei_api/src/anon_xfr/config.rs
+++ zei_api/src/anon_xfr/config.rs
@@ -4,4 +4,7 @@ const ASSET_TYPE_FRA: AssetType = AssetType([0; ASSET_TYPE_LENGTH]);
 pub const FEE_TYPE: AssetType = ASSET_TYPE_FRA;
 
 pub const FEE_CALCULATING_FUNC: fn(u32, u32) -> u32 =
-    |x: u32, y: u32| 50_0000 + 10_0000 * x + 20_0000 * y;
+    |x: u32, y: u32| {
+        let extra_outputs = y.checked_sub(x).unwrap_or(0);
+        50_0000 + 10_0000 * x + 20_0000 * y + (10_0000 * extra_outputs)
+    };
