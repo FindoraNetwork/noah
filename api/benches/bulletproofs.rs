dfae388d2c02f128cb581f6b8d6e52b2b5b93a98
孙如绿叶 <778398626@qq.com>
2022-09-21 14:25:54 +0800

    update

--- api/benches/bulletproofs.rs
+++ api/benches/bulletproofs.rs
@@ -1,23 +1,36 @@
-use std::time::Instant;
-
 use ark_std::test_rng;
-use bulletproofs::r1cs::{batch_verify, Prover, R1CSProof, Verifier};
-use bulletproofs::BulletproofGens;
-use bulletproofs::PedersenGens;
+use ark_std::time::Instant;
+use bulletproofs::{
+    r1cs::{batch_verify, Prover, R1CSProof, Verifier},
+    BulletproofGens, PedersenGens, RangeProof,
+};
 use merlin::Transcript;
 use mix::MixValue;
 use zei::setup::BulletproofParams;
 use zei::xfr::asset_mixer::{prove_asset_mixing, AssetMixingInstance};
-use zei_algebra::prelude::*;
-use zei_algebra::ristretto::{CompressedRistretto, RistrettoScalar};
-use zei_crypto::basic::pedersen_comm::{PedersenCommitment, PedersenCommitmentRistretto};
-use zei_crypto::bulletproofs::mix::{self, MixCommitment, MixVariable};
+use zei_algebra::{
+    prelude::*,
+    ristretto::{CompressedRistretto, RistrettoScalar},
+};
+use zei_crypto::{
+    basic::pedersen_comm::{PedersenCommitment, PedersenCommitmentRistretto},
+    bulletproofs::mix::{self, MixCommitment, MixVariable},
+};
 
-// Measurement of the verification time and batch verification time of Range/Mix Bulletproofs in Zei
 fn main() {
+    // Measurement of the verification time and batch verification time of Mix Bulletproofs in Zei
     bench_verify_asset_mixer();
     for i in 1..10 {
         bench_batch_verify_asset_mixer(i);
+    }
+
+    // Measurement of the verification time and batch verification time of Range Bulletproofs in Zei
+    bench_verify_range();
+    for i in [1, 2, 4, 8, 16] {
+        bench_multiple_verify_range(i)
+    }
+    for i in [1, 2, 4, 8, 16] {
+        bench_batch_verify_range(i);
     }
 }
 
@@ -56,7 +69,7 @@ fn bench_verify_asset_mixer() {
         assert!(v.0.verify(&v.1, &pc_gens, &bp_circuit_gens).is_ok());
     }
     println!(
-        "verify mix bulletproofs takes time: {} s",
+        "non-batch verify mix bulletproofs takes time: {} s",
         start.elapsed().as_secs_f32() / COUNT as f32
     );
 }
@@ -128,12 +141,156 @@ fn bench_batch_verify_asset_mixer(count: usize) {
         params.increase_circuit_gens(max_circuit_size);
     }
     let pc_gens = PedersenGens::default();
-    
+
     let start = Instant::now();
     assert!(batch_verify(&mut prng, verifiers, &pc_gens, &params.bp_circuit_gens).is_ok());
     println!(
-        "batch verify of {} mix bulletproofs takes time: {} s", count,
+        "batch verify of {} mix bulletproofs takes time: {} s",
+        count,
         start.elapsed().as_secs_f32() / count as f32
+    );
+}
+
+fn bench_verify_range() {
+    const COUNT: usize = 20;
+    let mut prng = test_rng();
+    let pc_gens = PedersenGens::default();
+    let bp_gens = BulletproofGens::new(64, 1);
+    let mut proofs = vec![];
+    let mut verifier_transcripts = vec![];
+
+    for _ in 0..COUNT {
+        let value = prng.gen_range(1u64..1000);
+        let blinding = RistrettoScalar::random(&mut prng).0;
+        let mut prover_transcript = Transcript::new(b"test");
+        let (proof, committed_value) = RangeProof::prove_single(
+            &bp_gens,
+            &pc_gens,
+            &mut prover_transcript,
+            value,
+            &blinding,
+            64,
+        )
+        .unwrap();
+        proofs.push((proof, committed_value));
+
+        let verifier_transcript = Transcript::new(b"test");
+        verifier_transcripts.push(verifier_transcript);
+    }
+
+    let start = Instant::now();
+    for ((proof, committed_value), transcipt) in proofs.iter().zip(verifier_transcripts.iter_mut())
+    {
+        assert!(proof
+            .verify_single(&bp_gens, &pc_gens, transcipt, &committed_value, 64)
+            .is_ok());
+    }
+    println!(
+        "single verify range bulletproofs takes time: {} s",
+        start.elapsed().as_secs_f32() / COUNT as f32
+    );
+}
+
+fn bench_multiple_verify_range(count: usize) {
+    let mut prng = test_rng();
+    let pc_gens = PedersenGens::default();
+    let bp_gens = BulletproofGens::new(64, 16);
+    let mut values = vec![];
+    let mut blindings = vec![];
+
+    for _ in 0..count {
+        let value = prng.gen_range(1u64..1000);
+        let blinding = RistrettoScalar::random(&mut prng).0;
+        values.push(value);
+        blindings.push(blinding);
+    }
+
+    let mut prover_transcript = Transcript::new(b"test");
+    let (proof, committed_value) = RangeProof::prove_multiple(
+        &bp_gens,
+        &pc_gens,
+        &mut prover_transcript,
+        &values,
+        &blindings,
+        64,
+    )
+    .unwrap();
+
+    let mut verifier_transcript = Transcript::new(b"test");
+    let start = Instant::now();
+    assert!(proof
+        .verify_multiple(
+            &bp_gens,
+            &pc_gens,
+            &mut verifier_transcript,
+            &committed_value,
+            64,
+        )
+        .is_ok());
+    println!(
+        "multiple verify of {} range bulletproofs takes time: {} s",
+        count,
+        start.elapsed().as_secs_f32() / count as f32
+    );
+}
+
+fn bench_batch_verify_range(count: usize) {
+    let mut prng = test_rng();
+    let bp_gens = BulletproofGens::new(64, 16);
+    let pc_gens = PedersenGens::default();
+    let mut proofs = vec![];
+    let mut committed_values = vec![];
+    let mut verifier_transcripts = vec![];
+
+    for _ in 0..count {
+        let mut values = vec![];
+        let mut blindings = vec![];
+
+        for _ in 0..4 {
+            let value = prng.gen_range(1u64..1000);
+            let blinding = RistrettoScalar::random(&mut prng).0;
+            values.push(value);
+            blindings.push(blinding);
+        }
+
+        let mut prover_transcript = Transcript::new(b"test");
+        let (proof, committed_value) = RangeProof::prove_multiple(
+            &bp_gens,
+            &pc_gens,
+            &mut prover_transcript,
+            &values,
+            &blindings,
+            64,
+        )
+        .unwrap();
+
+        proofs.push(proof);
+        committed_values.push(committed_value);
+
+        let verifier_transcript = Transcript::new(b"test");
+        verifier_transcripts.push(verifier_transcript);
+    }
+
+    let mut committed_values_ref = vec![];
+    for i in &committed_values {
+        committed_values_ref.push(i.as_slice())
+    }
+
+    let start = Instant::now();
+    assert!(RangeProof::batch_verify(
+        &mut prng,
+        &proofs.iter().map(|x| x).collect::<Vec<&RangeProof>>(),
+        &mut verifier_transcripts,
+        &committed_values_ref,
+        &bp_gens,
+        &pc_gens,
+        64,
+    )
+    .is_ok());
+    println!(
+        "batch verify of {} range bulletproofs takes time: {} s",
+        count,
+        start.elapsed().as_secs_f32() / (count * 4) as f32
     );
 }
 
@@ -281,15 +438,15 @@ fn asset_mix_num_generators(n_input: usize, n_output: usize) -> usize {
     let min = std::cmp::min(n_input, n_output);
 
     let input_wires = n_input + n_output;
-    let pad = max - min; 
+    let pad = max - min;
     let shuffle_input = 3 * n_input - 2;
-    let shuffle_output = 3 * n_output - 2; 
-    let shuffle_mid = 3 * max - 2; 
-    let merge_input_mid_wires = n_input - 2; 
-    let merge_output_mid_wires = n_output - 2; 
-    let merge_input = 2 * n_input - 1; 
-    let merge_output = 2 * n_output - 1; 
-    let range_proof = 64 * n_output; 
+    let shuffle_output = 3 * n_output - 2;
+    let shuffle_mid = 3 * max - 2;
+    let merge_input_mid_wires = n_input - 2;
+    let merge_output_mid_wires = n_output - 2;
+    let merge_input = 2 * n_input - 1;
+    let merge_output = 2 * n_output - 1;
+    let range_proof = 64 * n_output;
 
     input_wires
         + pad
