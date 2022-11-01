d8e71a58948231998f4703a95167d07f4575f24a
Weikeng Chen <w.k@berkeley.edu>
2022-11-01 13:21:42 -0700

    Split the CI for each crate (#199)
    * ci
    
    * fix
    
    * fix
    
    * cargo nextest
    
    * parallel feature smoke test
    
    * typo
    
    * comments
    
    Co-authored-by: onewayfunc <onewayfunc@gmail.com>

---
+++ smoke-tests/src/tests/mod.rs
@@ -0,0 +1,10 @@
+mod abar_merkle_tree;
+mod smoke_axfr;
+mod smoke_axfr_compatibility;
+mod smoke_axfr_secp256k1_address;
+mod smoke_xfr;
+mod smoke_xfr_compatibility;
+mod smoke_xfr_identity;
+mod smoke_xfr_secp256k1_address;
+mod smoke_xfr_tracing;
+mod xfr_note_complex;
