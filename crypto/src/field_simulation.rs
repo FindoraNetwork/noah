36b669e25dd9de165614d8250803f6334996aae1
孙如绿叶 <39479965+confuseSUN@users.noreply.github.com>
2022-11-02 00:50:54 +0800

    Add zorro curve nonnative parameters (#195)

--- crypto/src/field_simulation.rs
+++ crypto/src/field_simulation.rs
@@ -132,6 +132,55 @@ impl SimFrParams for SimFrParamsSecq256k1 {
     }
 }
 
+/// The parameters for field simulation for the zorro scalar field.
+#[derive(Clone, Default, Eq, PartialEq, Debug)]
+pub struct SimFrParamsZorro;
+
+impl SimFrParams for SimFrParamsZorro {
+    const NUM_OF_LIMBS: usize = 6;
+    const BIT_PER_LIMB: usize = 44;
+    const BIT_IN_TOP_LIMB: usize = 36;
+    const NUM_OF_GROUPS: usize = 6;
+
+    fn scalar_field_in_biguint() -> BigUint {
+        BigUint::from_str(
+            "57896044618658097711785492504343953926634992332820282019728792003956564819949",
+        )
+        .unwrap()
+    }
+
+    fn scalar_field_in_limbs() -> Vec<BLSScalar> {
+        [
+            BLSScalar::from_str("17592186044397").unwrap(),
+            BLSScalar::from_str("17592186044415").unwrap(),
+            BLSScalar::from_str("17592186044415").unwrap(),
+            BLSScalar::from_str("17592186044415").unwrap(),
+            BLSScalar::from_str("17592186044415").unwrap(),
+            BLSScalar::from_str("34359738367").unwrap(),
+        ]
+        .to_vec()
+    }
+
+    fn scalar_field_sub_pad_in_limbs() -> Vec<BLSScalar> {
+        [
+            BLSScalar::from_str("35184372088794").unwrap(),
+            BLSScalar::from_str("35184372088830").unwrap(),
+            BLSScalar::from_str("35184372088830").unwrap(),
+            BLSScalar::from_str("35184372088830").unwrap(),
+            BLSScalar::from_str("35184372088830").unwrap(),
+            BLSScalar::from_str("68719476734").unwrap(),
+        ]
+        .to_vec()
+    }
+
+    fn scalar_field_sub_pad_in_biguint() -> BigUint {
+        BigUint::from_str(
+            "115792089237316195423570985008687907853269984665640564039457584007913129639898",
+        )
+        .unwrap()
+    }
+}
+
 /// A precise indicator of the reducibility in a simulate element.
 #[derive(Eq, PartialEq, Clone)]
 pub enum SimReducibility {
@@ -706,6 +755,129 @@ mod test_secq256k1 {
     fn test_enforce_zero_panic() {
         let mut prng = test_rng();
         let r_biguint = SimFrParamsSecq256k1::scalar_field_in_biguint();
+
+        let a = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+        let b = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+
+        let a_fr = SimFrTest::from(&a);
+        let b_fr = SimFrTest::from(&b);
+
+        let ab_fr_mul = &a_fr * &b_fr;
+        let ab_fr = &a * &b;
+        assert_eq!(ab_fr, (&ab_fr_mul).into());
+
+        let ab_fr_reduced_manipulated = &ab_fr % &r_biguint + &BigUint::from(10u64);
+        let ab_reduced_manipulated = SimFrTest::from(&ab_fr_reduced_manipulated);
+
+        let zero_supposed_manipulated = &ab_fr_mul - &ab_reduced_manipulated;
+        zero_supposed_manipulated.enforce_zero();
+    }
+}
+
+#[cfg(test)]
+mod test_zorro {
+    use crate::field_simulation::{SimFr, SimFrParams, SimFrParamsZorro};
+    use noah_algebra::prelude::*;
+    use num_bigint::{BigUint, RandBigInt};
+    use num_integer::Integer;
+
+    type SimFrTest = SimFr<SimFrParamsZorro>;
+
+    #[test]
+    fn test_sim_fr_biguint_conversion() {
+        let mut prng = test_rng();
+        let r_biguint = SimFrParamsZorro::scalar_field_in_biguint();
+
+        for _ in 0..100 {
+            let a = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+            let a_sim_fr = SimFrTest::from(&a);
+            let a_recovered: BigUint = (&a_sim_fr).into();
+
+            assert_eq!(a, a_recovered);
+        }
+    }
+
+    #[test]
+    fn test_sub() {
+        let mut prng = test_rng();
+        let r_biguint = SimFrParamsZorro::scalar_field_in_biguint();
+
+        for _ in 0..100 {
+            let a = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+            let b = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+
+            let a_sim_fr = SimFrTest::from(&a);
+            let b_sim_fr = SimFrTest::from(&b);
+            let sum_sim_fr = &a_sim_fr - &b_sim_fr;
+
+            let (_, sum) = a.add(&r_biguint).sub(&b).div_rem(&r_biguint);
+            let (_, sum_recovered) =
+                <&SimFrTest as Into<BigUint>>::into(&sum_sim_fr).div_rem(&r_biguint);
+
+            assert_eq!(sum, sum_recovered);
+        }
+    }
+
+    #[test]
+    fn test_mul() {
+        let mut prng = test_rng();
+        let r_biguint = SimFrParamsZorro::scalar_field_in_biguint();
+
+        for _ in 0..100 {
+            let a = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+            let b = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+
+            let a_sim_fr = SimFrTest::from(&a);
+            let b_sim_fr = SimFrTest::from(&b);
+
+            let prod_sim_fr_mul = a_sim_fr.mul(&b_sim_fr);
+            let prod_sim_fr_mul_recovered: BigUint = (&prod_sim_fr_mul).into();
+
+            let prod = &a * &b;
+
+            assert_eq!(prod, prod_sim_fr_mul_recovered);
+        }
+    }
+
+    #[test]
+    fn test_enforce_zero_trivial() {
+        let zero_fr = SimFrTest::from(&BigUint::zero());
+        let zero_fr_mul = (&zero_fr) * (&zero_fr);
+
+        zero_fr_mul.enforce_zero();
+    }
+
+    #[test]
+    fn test_enforce_zero() {
+        let mut prng = test_rng();
+        let r_biguint = SimFrParamsZorro::scalar_field_in_biguint();
+
+        for _ in 0..1000 {
+            let a = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+            let b = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
+
+            let a_fr = SimFrTest::from(&a);
+            let b_fr = SimFrTest::from(&b);
+
+            let ab_fr_mul = &a_fr * &b_fr;
+            let ab_fr = &a * &b;
+            assert_eq!(ab_fr, (&ab_fr_mul).into());
+
+            let ab_fr_reduced = &ab_fr % &r_biguint;
+            let ab_reduced = SimFrTest::from(&ab_fr_reduced);
+
+            let zero_supposed = &ab_fr_mul - &ab_reduced;
+            let zero_supposed_biguint: BigUint = (&zero_supposed).into();
+            assert_eq!(BigUint::zero(), &zero_supposed_biguint % &r_biguint);
+            zero_supposed.enforce_zero();
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_enforce_zero_panic() {
+        let mut prng = test_rng();
+        let r_biguint = SimFrParamsZorro::scalar_field_in_biguint();
 
         let a = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
         let b = prng.gen_biguint_range(&BigUint::zero(), &r_biguint);
