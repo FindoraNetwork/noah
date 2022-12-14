b7cb3e775c7ca9a6c9f0c1e2788864bb89a50647
Sun <sun@findora.org>
2022-12-15 00:20:06 +1300

    Fix fr from_sign_and_limbs

--- algebra/src/bls12_381/fr.rs
+++ algebra/src/bls12_381/fr.rs
@@ -45,15 +45,7 @@ impl FromStr for BLSScalar {
 impl BLSScalar {
     /// Create a new scalar element from the arkworks-rs representation.
     pub const fn new(is_positive: bool, limbs: &[u64]) -> Self {
-        BLSScalar(Fr::GENERATOR)
-        // type Params = <Fr as PrimeField>::Params;
-        // BLSScalar(Fr::const_from_str(
-        //     &limbs,
-        //     is_positive,
-        //     Params::R2,
-        //     Params::MODULUS,
-        //     Params::INV,
-        // ))
+        BLSScalar(Fr::from_sign_and_limbs(is_positive, &limbs))
     }
 }
 
