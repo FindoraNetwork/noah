9f4ee8f61d136e16ce66af92559441ff4b732034
Sun <sun@findora.org>
2023-04-10 11:16:15 +1200

    Generate asset type from auditor

--- api/src/t_anon_xfr/structs.rs
+++ api/src/t_anon_xfr/structs.rs
@@ -1,3 +1,55 @@
+use crate::xfr::structs::{AssetType, ASSET_TYPE_LENGTH};
+use noah_algebra::{
+    bls12_381::BLSScalar,
+    jubjub::{JubjubPoint, JubjubScalar},
+    prelude::*,
+    traits::Domain,
+};
+use noah_crypto::basic::anemoi_jive::{AnemoiJive, AnemoiJive381};
+
+impl AssetType {
+    /// Generate asset type with auditor public key
+    pub fn generate<R: CryptoRng + RngCore>(
+        prng: &mut R,
+        pk: &AuditorPublicKey,
+    ) -> Result<(AssetType, BLSScalar)> {
+        let sample = BLSScalar::random(prng);
+        let pk_x = BLSScalar::from_field((pk.0).0.x);
+        let pk_y = BLSScalar::from_field((pk.0).0.y);
+
+        let trace = AnemoiJive381::eval_variable_length_hash_with_trace(&[pk_x, pk_y, sample]);
+        let output = trace.output.to_bytes();
+
+        let mut bytes = [0u8; ASSET_TYPE_LENGTH];
+        if ASSET_TYPE_LENGTH != output.len() {
+            return Err(eg!(NoahError::ParameterError));
+        }
+        bytes.copy_from_slice(&output);
+
+        Ok((AssetType(bytes), sample))
+    }
+}
+
 /// An auditor’s memo that accurately describes contents of the transactions.
 #[derive(Clone, Default, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct TAxfrAuditorMemo(Vec<u8>);
+
+/// The secret key for auditor.
+#[derive(Debug)]
+pub struct AuditorSecretKey(JubjubScalar);
+
+/// The public key for auditor.
+#[derive(Clone, Copy, Debug)]
+pub struct AuditorPublicKey(JubjubPoint);
+
+impl TAxfrAuditorMemo {
+    /// todo
+    pub fn encrypt() -> Result<Self> {
+        todo!()
+    }
+
+    /// todo
+    pub fn decrypt() -> Result<Vec<u8>> {
+        todo!()
+    }
+}
