5d0ea2fd388042fcd1395b8fa959e233484d343e
weikengchen <w.k@berkeley.edu>
2023-04-11 02:37:08 -0700

    tests

--- crypto/src/basic/anemoi_jive/tests.rs
+++ crypto/src/basic/anemoi_jive/tests.rs
@@ -1,5 +1,8 @@
-use super::*;
+use crate::basic::anemoi_jive::{AnemoiJive, AnemoiJive381, ApplicableMDSMatrix, MDSMatrix};
+use noah_algebra::bls12_381::BLSScalar;
 use noah_algebra::new_bls12_381;
+use noah_algebra::prelude::Scalar;
+use num_traits::{One, Zero};
 
 #[test]
 fn test_jive() {
