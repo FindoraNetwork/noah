bd4bc0a1fc8f7904d0ae3788e81d682981f1638d
Harshad Patil <harshad0402@gmail.com>
2023-05-09 22:10:23 -0700

    SW solutions of x

--- crypto/src/hashing_to_the_curve/traits.rs
+++ crypto/src/hashing_to_the_curve/traits.rs
@@ -1,19 +1,65 @@
 use noah_algebra::traits::Scalar;
+use ruc::*;
 
 /// Trait for the Shallue-van de Woestijne map
 pub trait SW<S: Scalar> {
     /// Constant Z0 of Shallue-van de Woestijne map
     const Z0: S;
-    /// Constant Z0 of Shallue-van de Woestijne map
+    /// Constant C1 of Shallue-van de Woestijne map
     const C1: S;
-    /// Constant Z0 of Shallue-van de Woestijne map
+    /// Constant C2 of Shallue-van de Woestijne map
     const C2: S;
-    /// Constant Z0 of Shallue-van de Woestijne map
+    /// Constant C3 of Shallue-van de Woestijne map
     const C3: S;
-    /// Constant Z0 of Shallue-van de Woestijne map
+    /// Constant C4 of Shallue-van de Woestijne map
     const C4: S;
-    /// Constant Z0 of Shallue-van de Woestijne map
+    /// Constant C5 of Shallue-van de Woestijne map
     const C5: S;
-    /// Constant Z0 of Shallue-van de Woestijne map
+    /// Constant C6 of Shallue-van de Woestijne map
     const C6: S;
+
+    /// first candidate for solution x
+    fn x1(&self, t: S) -> Result<S> {
+        let t_sq_inv = t.square().inv()?;
+        let c3t_sq_inv = Self::C3.mul(t_sq_inv);
+        let temp = S::one().add(c3t_sq_inv);
+
+        let temp2 = Self::C2.mul(temp.inv()?);
+        Ok(Self::C1.sub(&temp2))
+    }
+
+    /// second candidate for solution x
+    fn x2(&self, x1: &S) -> S {
+        Self::C4.sub(x1)
+    }
+
+    /// third candidate for solution x
+    fn x3(&self, t: S) -> Result<S> {
+        let t_sq = t.square();
+        let t_sq_inv = t_sq.inv()?;
+        let c3t_sq_inv = Self::C3.mul(t_sq_inv);
+        let temp = S::one().add(c3t_sq_inv);
+
+        let temp2 = t_sq.mul(temp.square());
+
+        Ok(Self::C5.add(Self::C6.mul(temp2)))
+    }
+}
+
+/// Trait for the simplified SWU map
+pub trait SSWU<S: Scalar> {
+    /// Constant k_1,0 of simplified SWU map
+    const K10: S;
+    /// Constant k_1,1 of simplified SWU map
+    const K11: S;
+    /// Constant k_1,2 of simplified SWU map
+    const K12: S;
+    /// Constant k_1,3 of simplified SWU map
+    const K13: S;
+    /// Constant k_2,0 of simplified SWU map
+    const K20: S;
+    /// Constant k_2,1 of simplified SWU map
+    const K21: S;
+    /// Constant k_2,2 of simplified SWU map
+    const K22: S;
 }
