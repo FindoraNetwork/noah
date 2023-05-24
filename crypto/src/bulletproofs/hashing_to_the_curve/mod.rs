1cc704926af618257b5f39e54862e57c8bfe4b4b
weikeng <cusgadmin@Weikengs-MacBook-Pro.local>
2023-05-23 19:02:34 -0700

    init

---
+++ crypto/src/bulletproofs/hashing_to_the_curve/mod.rs
@@ -0,0 +1,4 @@
+//! Module for hashing to the curve.
+
+pub mod secp256k1_sswu;
+pub mod ed25519_elligator;
--- crypto/src/bulletproofs/mod.rs
+++ crypto/src/bulletproofs/mod.rs
@@ -1,3 +1,4 @@
 pub mod mix;
 pub mod range;
 pub mod scalar_mul;
+pub mod hashing_to_the_curve;
