57b8d6f17e5722595e6bd2c9abe7353b48e658dc
Weikeng Chen <w.k@berkeley.edu>
2023-06-21 00:41:41 -0700

    Backtrace is now stable (#301)
    * edit
    
    * remove build.rs

--- plonk/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    if rustc_version::version_meta().unwrap().channel == rustc_version::Channel::Nightly {
-        println!("cargo:rustc-cfg=nightly");
-    }
-}
